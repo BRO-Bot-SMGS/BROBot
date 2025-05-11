c90a8cb5a77ed77cd46280f3c90126fe1a0e8f83
baj810 <billyjoachim8@gmail.com>
2025-05-11 19:50:37 +1000

    this works?? (at least the screens do)

--- program.ino
+++ program.ino
@@ -12,7 +12,7 @@
 
 //pins
     // led
-#define LED_RED_PIN 44 //pwm pin
+#define LED_RED_PIN 10 //pwm pin
 #define LED_GREEN_PIN 45 //pwm pin
 #define LED_BLUE_PIN 46 //pwm pin
     //motor
@@ -26,7 +26,7 @@
 #define ULTRASONIC_TRIG 2
 #define ULTRASONIC_ECHO 4
 #define CALIBRATION_SWITCH 2 //interupt pin
-#define PROGRAM_SWITCH 18 //interupt pin
+#define PROGRAM_SWITCH 26 //interupt pin
 #define SETTINGS_SWITCH 19 //interupt pin - still available 20, 21
 // ultrasonic sensor
 #define ULTRASONIC_TRIG 2
@@ -72,18 +72,19 @@ const uint8_t sensorPins[sensorCount] = {
 #define RGB_I2C_ADDRESS 0x05
 #define TOF_I2C_ADDRESS 0x06
 #define MULTIPLEXER_I2C_ADDRESS 0x70
-#define _I2C_ADDRESS 0x08
 
 #define OLED_1_MULTIPLEXER_PORT 0
-#define OLED_2_MULTIPLEXER_PORT 2
-#define RGB_I2C_PORT 3
-#define TOF_I2C_PORT 6
+#define OLED_2_MULTIPLEXER_PORT 1
+#define RGB_LEFT_MULTIPLEXER_PORT 3
+#define RGB_RIGHT_MULTIPLEXER_PORT 4
+#define TOF_HOLDER_MULTIPLEXER_PORT 5
+#define TOF_FRONT_MULTIPLEXER_PORT 2
 
 
 //settings
 //PID SETTINGS
 #define kp 0.05 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define kd 2 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define kd 2 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd)
 #define ki 0.001 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
 //speeds out of 255
 #define rightMaxSpeed 200 // max speed of the robot
@@ -126,7 +127,7 @@ float duration, distance;
 bool calibrationSwitchState = false;
 #define NUM_BUTTONS 3
 #define NUM_SENSORS 13
-#define targetPosition 3500 // Adjust this value based on your sensor configuration
+#define targetPosition 7000 // Adjust this value based on your sensor configuration
 float serialSpeed = 115200; // 4800, 9600, 38400, 115200
 String robotStatus = "Idle";
 bool systemReady = false;
@@ -141,10 +142,21 @@ bool programDisplayAlertStatus = false;
 String programDisplayAlert = "Alert Test";
 bool lineLost = false;
 int sensorValues[SENSOR_ARRAY_SIZE];
+int position = 0;
+int error = 0;
+int leftSpeed = 0;
+int rightSpeed = 0;
+int rapidDisplayRefreshDelay = 300;
+int eepromCalibrationOffset = 1500;
+#define EEPROM_MAGIC 0xDEAD
+#define EEPROM_ADDR_MAGIC (eepromCalibrationOffset)
+#define EEPROM_ADDR_MIN (eepromCalibrationOffset + sizeof(uint16_t))
+#define EEPROM_ADDR_MAX (EEPROM_ADDR_MIN + NUM_SENSORS * sizeof(uint16_t))
 //eeprom addresses
 #define lastCalibrationAmountAddress 101
 #define lastCalibrationTimeAddress 102
 #define lastCalibrationDataAddress 103
+#define EEPROMindex 100
 
 //global setup
     // multiplexer setup
@@ -153,7 +165,7 @@ int sensorValues[SENSOR_ARRAY_SIZE];
     // For QTRSensorsAnalog
 // QTRSensors qtra;
 QTRSensors qtra;
-// QTRSensorsAnalog qtra(sensorPins, sensorCount, EMITTER_PIN, timeout); 
+// QTRSensorsAnalog qtra(sensorPins, sensorCount, EMITTER_PIN, timeout);
 // QTRSensors qtra((unsigned char[]) {
 //     SENSOR_ARRAY_1, SENSOR_ARRAY_2, SENSOR_ARRAY_3, SENSOR_ARRAY_4, SENSOR_ARRAY_5,
 //     SENSOR_ARRAY_6, SENSOR_ARRAY_7, SENSOR_ARRAY_8, SENSOR_ARRAY_9, SENSOR_ARRAY_10,
@@ -202,7 +214,7 @@ void setPinModes(){
 
 void initSensorArray(int emitterPin) {
     qtra.setTypeAnalog();
-    qtra.setSensorPins(sensorPins, sensorCount); // Use sensorPins array
+    qtra.setSensorPins((const uint8_t[]){A15, A14, A13, A12, A11, A10, A9, A8, A5, A4, A3, A2, A1}, sensorCount);
     qtra.setEmitterPin(emitterPin);
     Serial.println("Sensor array initialized");
 }
@@ -229,6 +241,53 @@ void resetDefaults(){
 void retrieveEEPROMvalues() {
 EEPROM.get(lastCalibrationTimeAddress, calibrationTime);
 EEPROM.get(lastCalibrationAmountAddress, calibrationAmount);
+}
+
+void storeCalibrationDataToEEPROM() {
+    int initialMinAddr = EEPROMindex;
+    int initialMaxAddr = EEPROMindex + NUM_SENSORS + 1;
+    int initialInfoAddr = EEPROMindex + NUM_SENSORS * 2 + 1;
+    // total eeprom addresses should be 31?
+    EEPROM.put(EEPROM_ADDR_MAGIC, EEPROM_MAGIC);
+
+    for (int i = 0; i < NUM_SENSORS; i++) { // store eeprom min values
+        int addr = initialMinAddr + i;
+        EEPROM.put(addr, qtra.calibrationOn.minimum[i]);
+    }
+
+    for (int i = 0; i < NUM_SENSORS; i++) { // store eeprom max values
+        int addr = initialMaxAddr + i;
+        EEPROM.put(addr, qtra.calibrationOn.maximum[i]);
+    }
+
+    // calibration info store
+    // 1 - calibration amount
+    // 2 - calibration time
+    // 3 - calibration valid
+
+    EEPROM.put(initialInfoAddr, calibrationTime); // calibration time
+    EEPROM.put(initialInfoAddr+1, calibrationAmount); // calibration amount
+    EEPROM.put(initialInfoAddr+2, 1); // calibration valid
+
+    Serial.println("Calibration data successfully stored to EEPROM.");
+}
+
+bool retrieveCalibrationDataFromEEPROM() {
+    Serial.println("Looking for EEPROM calibration data...");
+
+    // if (EEPROM.read(initialInfoAddr+2) != true) {
+    //     Serial.println("No valid calibration data found in EEPROM.");
+    //     return false; // No valid calibration data found
+    // }
+
+    for (int i = 0; i < NUM_SENSORS; i++) { // load eeprom min values
+        int addr = EEPROM_ADDR_MIN + i;
+        EEPROM.get(addr, qtra.calibrationOn. minimum[i]);
+        qtra.calibrationOn.minimum[i] = EEPROM.read(addr);
+    }
+
+    Serial.println("Calibration data loaded from EEPROM.");
+
 }
 
 int calculateUltrasonic(int trigPin, int echoPin) {
@@ -312,36 +371,50 @@ void displayProgramData(){
     selectMultiplexerPort(OLED_1_MULTIPLEXER_PORT);
     delay(20);
     display[0].clearDisplay();
-    if (programDisplayAlertStatus = true) {
-        display[0].drawRoundRect(0, 15, 22, 30, 5, WHITE);
-        display[0].setTextSize(1);
-        display[0].setCursor(15, 15);
-        display[0].println("255");
+    if (programDisplayAlertStatus = true) {;
+        display[0].setTextSize(2);
+        display[0].setCursor(22, 0);
+        display[0].println("ALERT");
         display[0].display();
     }
     // left wheel render
     display[0].setTextSize(2);
-    display[0].setCursor(15, 15);
+    display[0].setCursor(0, 0);
     display[0].println("L");
 
-    display[0].drawRoundRect(0, 30, 22, 30, 5, WHITE);
+    // display[0].drawRoundRect(x - TLH, y - TLH, width, height, roundedAmount, WHITE);
+    display[0].drawRoundRect(0, 20, 30, 30, 5, WHITE);
     display[0].setTextSize(1);
-    display[0].setCursor(42, 10);
-    display[0].println("255");
+    display[0].setCursor(2, 30);
+    display[0].println(leftSpeed);
     display[0].display();
 
     // right wheel render
-    display[0].setTextSize(1);
-    display[0].setCursor(15, 15);
-    display[0].println("255");
+    display[0].setTextSize(2);
+    display[0].setCursor(116, 0);
+    display[0].println("R");
 
-    display[0].drawRoundRect(0, 15, 22, 30, 5, WHITE);
+    display[0].drawRoundRect(98, 20, 30, 30, 5, WHITE);
     display[0].setTextSize(1);
-    display[0].setCursor(15, 15);
-    display[0].println("255");
+    display[0].setCursor(100, 30);
+    display[0].println(rightSpeed);
+
+    // line array render
+    display[0].setTextSize(1);
+    display[0].setCursor(53, 16);
+    display[0].println("pos:");
+    display[0].setCursor(40, 25);
+    display[0].println(position);
+    display[0].setCursor(50, 40);
+    display[0].println("error:");
+    display[0].setCursor(40, 50);
+    display[0].println(error);
+
+
     display[0].display();
-
+    delay(rapidDisplayRefreshDelay);
 }
+
 
 void alertProgramDataDisplay(String alert){
     alert = programDisplayAlert;
@@ -431,7 +504,7 @@ void initMotors(){
     pinMode(MOTOR2_BRAKE, OUTPUT);
 }
 void setM1Speed(int speed){
-    if (speed < 0){
+    if (speed > 0){
         digitalWrite(MOTOR1_DIR, HIGH);
         analogWrite(MOTOR1_PWM, -speed);
     }
@@ -441,7 +514,7 @@ void setM1Speed(int speed){
     }
 }
 void setM2Speed(int speed){
-    if (speed < 0){
+    if (speed > 0){
         digitalWrite(MOTOR2_DIR, HIGH);
         analogWrite(MOTOR2_PWM, -speed);
     }
@@ -470,6 +543,10 @@ void setMotorSpeedsAndBrakes(int m1Speed, int m2Speed, int m1Brake, int m2Brake)
 }
 void stopMotors(){
     setMotorSpeedsAndBrakes(0, 0, 1, 1);
+}
+void unlockMotors(){
+    setMotorBrakes(0, 0);
+    Serial.println("Motors unlocked");
 }
 
 
@@ -549,6 +626,13 @@ void setup() {
     retrieveEEPROMvalues();
     initMotors();
     stopMotors();
+    setLEDColor(255, 0, 0); //red
+    delay(200);
+    setLEDColor(0, 255, 0); //green
+    delay(200);
+    setLEDColor(0, 0, 255); //blue
+    delay(200);
+    setLEDColor(100, 0, 255); //thinking color
 
 Serial.println("Selecting OLED 1 Port...");
 selectMultiplexerPort(OLED_1_MULTIPLEXER_PORT);
@@ -578,6 +662,7 @@ void loop() {
 
     if (calibrationSwitch.fell()) { // if the calibration switch is turned off do this once
         calibrationSwitchState = false;
+        setLEDColor(100, 0, 255); //thinking color
         selectMultiplexerPort(OLED_2_MULTIPLEXER_PORT);
         calibrationTime = millis() - calibrationStartTime;
         robotStatus = "UpdCalData";
@@ -585,13 +670,7 @@ void loop() {
         displayCalibrationData(1);
         Serial.println("Calibration switch switched to LOW, stopping calibration...");
         Serial.println("Saving calibration data to EEPROM...");
-        for (int i = 0; i < sensorCount; i++) { // store calibration data to EEPROM
-            longTermMemoryStore(i, calibratedMinimumOn[i]);
-            longTermMemoryStore(i + sensorCount, calibratedMaximumOn[i]);
-        }
-        longTermMemoryStore(lastCalibrationAmountAddress, calibrationAmount);
-        longTermMemoryStore(lastCalibrationTimeAddress, (millis() - calibrationStartTime));
-        Serial.println("Calibration data saved to EEPROM");
+        storeCalibrationDataToEEPROM();
         Serial.println("Calibration data:");
         Serial.println("Updating display with calibration data...");
         robotStatus = "Idle";
@@ -603,6 +682,7 @@ void loop() {
     }
 
     if (calibrationSwitch.rose()) { // if the calibration switch is turned on do this once
+        setLEDColor(100, 0, 255); //thinking color
         calibrationSwitchState = true;
         selectMultiplexerPort(OLED_2_MULTIPLEXER_PORT);
         Serial.println("Calibration switch switched to HIGH, calibrating...");
@@ -632,36 +712,50 @@ void loop() {
     bool programSwitchState = programSwitch.read() == HIGH;
 
     if (programSwitch.fell()) { // if the program switch is turned off do this once
-        updateDisplayStatus("Stpng Prog");
         stopMotors();
+        updateDisplayStatus("Stpng Prog");
+        Serial.println("Program switch switched to LOW, stopping program...");
+        setLEDColor(100, 0, 255); //thinking color
         updateDisplayStatus("Idle");
         programRunning = false;
         loadDefaultDisplay();
+        setMotorBrakes(1, 1); // lock brakes
+        setLEDColor(255, 255, 255);
     }
 
     if (programSwitch.rose()) { // if the program switch is turned on do this once
         updateDisplayStatus("Runng Prog");
+        Serial.println("Program switch switched to HIGH, starting program...");
+        setLEDColor(255, 255, 0);
         setMotorBrakes(0, 0); // unlock brakes
         programRunning = true;
         displayProgramData();
     }
 
-    if (programSwitchState) { // while the program switch is on do this (loop)
-    
-        uint16_t sensorValues[NUM_SENSORS];
-        int position = qtra.readLineBlack(sensorValues); // Use readLineWhite for a white line on a black background
+    if (programSwitchState == true) { // while the program switch is on do this (loop)
+
+    // pid line following
+        uint16_t sensorValues[NUM_SENSORS]; // Array to hold sensor values
+        uint16_t position = qtra.readLineBlack(sensorValues); // Get the position of the line
+        for (uint8_t i = 0; i < sensorCount; i++) // Loop through each sensor value and print them into the serial monitor
+        {
+            Serial.print(sensorValues[i]);
+            Serial.print('\t');
+        }
+        Serial.println(position);
+        
 
         // Calculate error.
-        int error = position - targetPosition;
+        int error = position - targetPosition; // Calculate error. - how far away it is from the line
 
         // Calculate integral.
-        integral += error;
+        integral += error; //cumulative sum of the errors over time. It accounts for past errors and helps eliminate residual steady-state errors.
 
         // Limit the integral term to prevent windup.
         integral = constrain(integral, -500, 500);
 
         // Calculate derivative.
-        int derivative = error - lastError;
+        int derivative = error - lastError; //is the rate of change of the error. It predicts future error based on its current rate of change, helping to dampen the system response and reduce overshoot.
 
         // Calculate PID output.
         float pidOutput = kp * error + ki * integral + kd * derivative;
@@ -674,39 +768,17 @@ void loop() {
         leftSpeed = constrain(leftSpeed, 0, 255);
         rightSpeed = constrain(rightSpeed, 0, 255);
 
-        // Control motors (replace with your motor control logic).
-        if (leftSpeed > leftBaseSpeed) {
-            setM1Speed(leftSpeed); // Forward
-        } else {
-            setM1Speed(-leftSpeed); // Reverse
-        }
+        Serial.print("Left speed: ");
+        Serial.println(leftSpeed);
+        Serial.print("Right speed: ");
+        Serial.println(rightSpeed);
 
-        if (rightSpeed > rightBaseSpeed) {
-            setM2Speed(rightSpeed); // Forward
-        } else {
-            setM2Speed(-rightSpeed); // Reverse
-        }
+
+        unlockMotors();
+        setM1Speed(leftSpeed); // Forward
+        setM2Speed(rightSpeed); // Forward
 
         // Update last error.
         lastError = error;
-    
-    }
-    
-    // look for obstacles
-    if (programRunning) {
-        // can_distance = calculateUltrasonic(ULTRASONIC_TRIG, ULTRASONIC_ECHO);
-        stopMotors();
-        }
-        else {
-            can_detected = false;
-        }
-
-    // look for green zone
-
-    if (programRunning && greenZoneDetected) {
-        stopMotors();
-    }
-    else {
-        greenZoneDetected = false;
     }
 }
